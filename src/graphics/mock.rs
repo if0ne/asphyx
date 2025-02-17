4410db85c89773af391da37de1678ec327d955d6
Pavel Agafonov <pavel.agafonov.al@gmail.com>
2025-02-17 23:50:26 +0300

    refactor: acranization

--- src/graphics/mock.rs
+++ src/graphics/mock.rs
@@ -3,15 +3,12 @@ use std::{
     sync::{atomic::AtomicU64, Arc},
 };
 
-use bytemuck::{cast_slice_mut, checked::cast_slice, Pod};
+use bytemuck::{checked::cast_slice, Pod};
 use parking_lot::Mutex;
 
-use super::{
-    types::{
-        BindGroupDesc, BindGroupLayoutDesc, BufferDesc, BufferUsage, CommandQueueType,
-        ComputePipelineDesc, ImageDesc, ImageViewDesc, RenderPipelineDesc, SyncPoint,
-    },
-    Handle, Pool,
+use super::types::{
+    BindGroupDesc, BindGroupLayoutDesc, BufferDesc, BufferUsage, CommandQueueType,
+    ComputePipelineDesc, ImageDesc, ImageViewDesc, RenderPipelineDesc, SyncPoint,
 };
 
 pub struct RenderBackend {
@@ -35,28 +32,12 @@ impl RenderBackend {
 }
 
 pub struct RenderDevice {
-    buffers: Mutex<Pool<Buffer>>,
-    images: Mutex<Pool<Image>>,
-    render_pipeline: Mutex<Pool<RenderPipeline>>,
-    compute_pipeline: Mutex<Pool<ComputePipeline>>,
-    bind_group_layout: Mutex<Pool<BindGroupLayout>>,
-    bind_group: Mutex<Pool<BindGroup>>,
-    temp_bind_group: Mutex<Pool<TemporaryBindGroup>>,
-
     io_queue: CommandQueue,
 }
 
 impl RenderDevice {
     pub fn new() -> Self {
         Self {
-            buffers: Mutex::new(Pool::new(None)),
-            images: Mutex::new(Pool::new(None)),
-            render_pipeline: Mutex::new(Pool::new(None)),
-            compute_pipeline: Mutex::new(Pool::new(None)),
-            bind_group_layout: Mutex::new(Pool::new(None)),
-            bind_group: Mutex::new(Pool::new(None)),
-            temp_bind_group: Mutex::new(Pool::new(None)),
-
             io_queue: CommandQueue::new(CommandQueueType::Io, None),
         }
     }
@@ -69,19 +50,18 @@ impl RenderDevice {
         Arc::new(CommandQueue::new(ty, cb_count))
     }
 
-    pub fn create_buffer<T: Pod>(&self, desc: BufferDesc<T>) -> Handle<Buffer> {
-        let mut buffer = Buffer {
-            buf: vec![0; desc.size],
+    pub fn create_buffer<T: Pod>(&self, desc: BufferDesc<T>) -> Arc<Buffer> {
+        let buffer = Arc::new(Buffer {
+            buf: Mutex::new(vec![0; desc.size]),
             size: desc.size,
             stride: desc.stride,
             usage: desc.usage,
-        };
+        });
 
-        let dst_buffer = if let Some(content) = desc.content {
+        if let Some(content) = desc.content {
             if !desc.usage.contains(BufferUsage::Copy) {
-                let mut cmd = self.io_queue.create_command_buffer(self);
+                let mut cmd = self.io_queue.create_command_buffer();
 
-                let dst_buffer = self.buffers.lock().push(buffer);
                 let src_buffer = self.create_buffer(BufferDesc {
                     usage: BufferUsage::Copy,
                     ..desc
@@ -89,59 +69,50 @@ impl RenderDevice {
 
                 {
                     let io_encoder = cmd.blit_encoder();
-                    io_encoder.copy_buffer_to_buffer(&self, dst_buffer, src_buffer);
+                    io_encoder.copy_buffer_to_buffer(&buffer, &src_buffer);
                 }
 
                 self.io_queue.push_cmd_buffer(cmd);
                 self.io_queue.commit();
-
-                dst_buffer
             } else {
-                buffer.buf.clone_from_slice(cast_slice(&content));
-
-                self.buffers.lock().push(buffer)
+                buffer.buf.lock().clone_from_slice(cast_slice(&content));
             }
-        } else {
-            self.buffers.lock().push(buffer)
         };
 
-        dst_buffer
+        buffer
     }
 
-    pub fn create_image(&mut self, desc: ImageDesc) -> Handle<Image> {
+    pub fn create_image(&mut self, desc: ImageDesc) -> Arc<Image> {
         todo!()
     }
 
-    pub fn create_image_view(&mut self, desc: ImageViewDesc) -> Handle<Image> {
+    pub fn create_image_view(&mut self, desc: ImageViewDesc) -> Arc<Image> {
         todo!()
     }
 
-    pub fn create_bind_group_layout(
-        &mut self,
-        desc: BindGroupLayoutDesc,
-    ) -> Handle<BindGroupLayout> {
+    pub fn create_bind_group_layout(&mut self, desc: BindGroupLayoutDesc) -> Arc<BindGroupLayout> {
         todo!()
     }
 
-    pub fn create_bind_group(&mut self, desc: BindGroupDesc) -> Handle<BindGroup> {
+    pub fn create_bind_group(&mut self, desc: BindGroupDesc) -> Arc<BindGroup> {
         todo!()
     }
 
-    pub fn create_temp_bind_group(&mut self, desc: BindGroupDesc) -> Handle<TemporaryBindGroup> {
+    pub fn create_temp_bind_group(&mut self, desc: BindGroupDesc) -> Arc<TemporaryBindGroup> {
         todo!()
     }
 
     pub fn create_render_pipeline_state(
         &mut self,
         desc: &RenderPipelineDesc,
-    ) -> Handle<RenderPipeline> {
+    ) -> Arc<RenderPipeline> {
         todo!()
     }
 
     pub fn create_compute_pipeline_state(
         &mut self,
         desc: &ComputePipelineDesc,
-    ) -> Handle<ComputePipeline> {
+    ) -> Arc<ComputePipeline> {
         todo!()
     }
 }
@@ -177,7 +148,7 @@ impl CommandQueue {
         }
     }
 
-    pub fn create_command_buffer(&self, _device: &RenderDevice) -> CommandBuffer {
+    pub fn create_command_buffer(&self) -> CommandBuffer {
         if let Some(buffer) = self.in_record.lock().pop() {
             return buffer;
         }
@@ -269,18 +240,8 @@ pub struct BlitEncoder<'a> {
 }
 
 impl<'a> BlitEncoder<'a> {
-    pub fn copy_buffer_to_buffer(
-        &self,
-        device: &RenderDevice,
-        dst: Handle<Buffer>,
-        src: Handle<Buffer>,
-    ) {
-        let mut guard = device.buffers.lock();
-
-        let src = { guard.get(src).expect("failed to get buffer").buf.clone() };
-        let dst = guard.get_mut(dst).expect("failed to get buffer");
-
-        dst.buf.clone_from_slice(&src);
+    pub fn copy_buffer_to_buffer(&self, dst: &Buffer, src: &Buffer) {
+        dst.buf.lock().clone_from_slice(&src.buf.lock());
     }
 }
 
@@ -290,7 +251,7 @@ pub struct ComputePipeline {}
 
 #[derive(Debug)]
 pub struct Buffer {
-    buf: Vec<u8>,
+    buf: Mutex<Vec<u8>>,
     size: usize,
     stride: usize,
     usage: BufferUsage,
