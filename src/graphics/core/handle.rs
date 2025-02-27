fec5296bc1f8da547fc3aec67022558b381e3718
Pavel Agafonov <pavel.agafonov.al@gmail.com>
2025-02-27 23:07:43 +0300

    added: render handle

---
+++ src/graphics/core/handle.rs
@@ -0,0 +1,110 @@
+use std::{hash::Hash, marker::PhantomData};
+
+use static_assertions::const_assert_eq;
+
+use crate::utils::non_max::NonMax;
+
+const_assert_eq!(
+    size_of::<Option<RenderHandle<usize>>>(),
+    size_of::<RenderHandle<usize>>()
+);
+
+pub struct RenderHandle<T> {
+    index: u32,
+    gen: NonMax<u32>,
+    _marker: PhantomData<T>,
+}
+
+impl<T> Clone for RenderHandle<T> {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+impl<T> Copy for RenderHandle<T> {}
+
+impl<T> Hash for RenderHandle<T> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.index.hash(state);
+    }
+}
+
+impl<T> PartialEq for RenderHandle<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.index == other.index && self.gen == other.gen
+    }
+}
+
+impl<T> Eq for RenderHandle<T> {}
+
+impl<T> std::fmt::Debug for RenderHandle<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("ResourceHandle")
+            .field("index", &self.index)
+            .field("gen", &self.gen())
+            .finish()
+    }
+}
+
+impl<T> RenderHandle<T> {
+    pub fn new(index: u32, gen: u32) -> Self {
+        Self {
+            index,
+            gen: NonMax::new(gen).expect("wrong gen"),
+            _marker: PhantomData,
+        }
+    }
+
+    pub fn idx(&self) -> u32 {
+        self.index
+    }
+
+    pub fn gen(&self) -> u32 {
+        self.gen.get()
+    }
+}
+
+#[derive(Debug)]
+pub struct RenderHandleAllocator<T> {
+    gens: Vec<u32>,
+    free_list: Vec<u32>,
+    _marker: PhantomData<T>,
+}
+
+impl<T> RenderHandleAllocator<T> {
+    #[inline]
+    pub fn new() -> Self {
+        Self {
+            gens: Vec::new(),
+            free_list: Vec::new(),
+            _marker: PhantomData,
+        }
+    }
+
+    #[inline]
+    pub fn allocate(&mut self) -> RenderHandle<T> {
+        if let Some(idx) = self.free_list.pop() {
+            RenderHandle::new(idx, self.gens[idx as usize])
+        } else {
+            let idx = self.gens.len();
+            self.gens.push(0);
+
+            RenderHandle::new(idx as u32, 0)
+        }
+    }
+
+    #[inline]
+    pub fn is_valid(&self, handle: RenderHandle<T>) -> bool {
+        self.gens
+            .get(handle.index as usize)
+            .is_some_and(|h| *h == handle.gen.get())
+    }
+
+    #[inline]
+    pub fn free(&mut self, handle: RenderHandle<T>) {
+        if let Some(gen) = self.gens.get_mut(handle.index as usize) {
+            *gen += 1;
+            self.free_list.push(handle.index);
+        }
+    }
+}
